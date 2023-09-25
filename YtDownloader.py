e431cd7b9f848e7abe5bd4ecbf4f573bfa519530
wirytiox <79142532+wirytiox@users.noreply.github.com>
2023-09-25 16:00:51 -0300

    Your commit message here

--- YtDownloader.py
+++ YtDownloader.py
@@ -1,87 +1,69 @@
 from tkinter import *
 from tkinter import ttk
-import tkinter
 from tkinter.ttk import *
 from tkinter import filedialog
 import os
-import youtube_dl
-from asyncio.log import logger
-import logging
-def debugMode():
-    CheckboxNotCriying=checkbox_var.get()
-    
-    if CheckboxNotCriying==True:
-        logging.basicConfig(filename='test.log',level=logging.DEBUG)
-        logging.debug('debugging is enabled')
-    else:
-        print('debugger is not enabled and you cant see this')
-        print(CheckboxNotCriying)
-directorio=desktop = os.path.expanduser("~/Desktop")
+import yt_dlp
+
 def browseFiles():
     global directorio
     directorio = filedialog.askdirectory()
+
 def downloadmp3():
     try:
         url = txt1.get()
+        output_path = os.path.join(directorio, '%(title)s.%(ext)s')
         ydl_opts = {
             'format': 'bestaudio/best',
-            'outtmpl': directorio+"\\"+'%(title)s.%(ext)s',
+            'outtmpl': output_path,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': '320'
+                'preferredquality': '320',
             }],
-            'extractaudio': True,
-            'audioformat': 'mp3',
-            'noplaylist': True,
-            'nocheckcertificate': True,
-            'quiet': False,
-            'no_warnings': False,
-            'default_search': 'auto'
         }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-    except :
-        logger.exception('you fucked up')
+    except Exception as e:
+        print(f'Error occurred while downloading MP3: {str(e)}')
+
 def downloadmp4():
     try:
         url = txt1.get()
+        output_path = os.path.join(directorio, '%(title)s.%(ext)s')
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'outtmpl': directorio+"\\"+'%(title)s.%(ext)s',
+            'outtmpl': output_path,
             'merge_output_format': 'mp4',
             'prefer_ffmpeg': True,
-            'postprocessors': [{
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4'
-            }]
         }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-    except:
-        logger.exception('Error occurred while downloading video.')
-window = Tk()                                                           #ClassDefinition
-window.title("Yotube Downloader")                                       #Titulo
-#window.iconbitmap("icon.ico")                                          #icon broken i dont know how to make it work
-checkbox_var = IntVar()                                                 #int var, odio esto
-checkbox = ttk.Checkbutton(window,
-                text='Debug Mode',
-                command=debugMode,
-                variable=checkbox_var,
-                onvalue=True,
-                offvalue=False).grid(column=0, row=3)
-lbl = Label (window, text="Youtube Downloader")                         #Label Decorativa
-lbl.grid(column=0, row=0)                                               #Label Decorativa
-btnmp3 = Button (window, text="Descargar Mp3",command=downloadmp3)     #Descargarmp3   boton
-btnmp3.grid(column=2, row=0)                                            #Descargarmp3   boton
-btnmp4 = Button(window, text="Descargar Mp4", command=downloadmp4)     #descargarMP4   boton
-btnmp4.grid(column=2, row=1)                                            #descargarMP4   boton
-txt1 = Entry(window,width=40)                                           #link
-txt1.grid(column=0, row=2)                                              #link
-txt1.get()                                                              #link
-btn = Button(window, text="Donde descargar", command=browseFiles)       #FileExplorer boton
-btn.grid(column=2, row=2)                                               #FileExplorer boton
+    except Exception as e:
+        print(f'Error occurred while downloading MP4: {str(e)}')
+
+window = Tk()
+window.title("YouTube Downloader")
+
+directorio = os.path.expanduser("~/Desktop")
+
+lbl = Label(window, text="YouTube Downloader")
+lbl.grid(column=0, row=0)
+
+btnmp3 = Button(window, text="Download MP3", command=downloadmp3)
+btnmp3.grid(column=2, row=0)
+
+btnmp4 = Button(window, text="Download MP4", command=downloadmp4)
+btnmp4.grid(column=2, row=1)
+
+txt1 = Entry(window, width=40)
+txt1.grid(column=0, row=2)
+
+btn = Button(window, text="Select Download Directory", command=browseFiles)
+btn.grid(column=2, row=2)
+
 window.mainloop()
+
 # notes
-# pyinstaller --onefile -w -F --icon="icon.ico" --add-binary "icon.ico;." "SimplePyDownloader.py"
-# pyinstaller --onefile -w -F "SimplePyDownloader.py"
+# pyinstaller --onefile -w -F --icon="icon.ico" --add-binary "icon.ico;." "YtDownloader.py"
+# pyinstaller --onefile -w -F "YtDownloader.py"
